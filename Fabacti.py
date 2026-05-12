7770f6b6a8389afd258d403056968b698952a563
LuisAlvarezS <86607669+LuisAlvarezS@users.noreply.github.com>
2026-05-12 09:36:58 -0500

    Refactor fabacti function header display
    Removed sidebar and updated header display in fabacti function.

--- Fabacti.py
+++ Fabacti.py
@@ -3,10 +3,8 @@ import streamlit as st
 
 def fabacti():
   # Encabezado
-  enc1, enc2 = st.columns([10,6])
-  enc1.write(" :red[FABACTI] :registered: :blue[Consultoría especializada en tecnologías de la información y las comunicaciones]")
-  #enc2.badge(fechapantalla(), icon = co.ICON_EVENTOS, color = 'blue')
-  st.sidebar.write(":copyright: 2024 Todos los derechos reservados de autor :red[FABACTI] :registered:")
+  st.write(" :red[FABACTI] :registered: :blue[Consultoría especializada en tecnologías de la información y las comunicaciones]")
+  st.write(":copyright: 2024 Todos los derechos reservados de autor :red[FABACTI] :registered:")
 
   st.write(' ... Vamos para adelante con Fabacti con toda ...')
   st.write(' .. cambios ...')
