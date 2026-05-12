ec3091d520f2205eb08de10a1d5b7865899f060f
LuisAlvarezS <86607669+LuisAlvarezS@users.noreply.github.com>
2026-05-12 19:37:49 +0000

    Pagina de documentacion

---
+++ pages/080Documentacion.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.write(" :red[NOTICIAS] :registered: :blue[Noticias y novedades sobre FABACTI]")
+st.write(":copyright: 2024 Todos los derechos reservados de autor :red[FABACTI] :registered:")  
