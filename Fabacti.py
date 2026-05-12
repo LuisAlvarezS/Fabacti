1c4689e3f8a2128ae4934fbc89a511120cd011b5
LuisAlvarezS <86607669+LuisAlvarezS@users.noreply.github.com>
2026-05-12 21:56:51 +0000

    Trm

--- Fabacti.py
+++ Fabacti.py
@@ -1,14 +1,34 @@
+import datetime
+
 import streamlit as st
+
+from suds.client import Client
+
+# Funcion para consultar el TRM dada una fecha
+def obtener_trm(fecha):
+    #URL de referencia para consulta de la TRM (Tasa Representativa del Mercado). Valor de referencia del dolar USA
+    URL_TRM = 'https://www.superfinanciera.gov.co/SuperfinancieraWebServiceTRM/TCRMServicesWebService/TCRMServicesWebService?WSDL'
+
+    try:
+        client = Client(URL_TRM, location=co.URL_TRM, faults=True)
+        trm =  client.service.queryTCRM(fecha)
+    except Exception as e:
+        return str(e)
+    return trm[4]
 
 
 def fabacti():
   # Encabezado
   st.write(" :red[FABACTI] :registered: :blue[Consultoría especializada en tecnologías de la información y las comunicaciones]")
-  st.write(":copyright: 2026 Todos los derechos reservados de autor :red[FABACTI] :registered:")
+  fecha = datetime.now()
+  trm = float(obtener_trm(fecha))
+  ftrm = '${:,.2f} '.format(trm)
 
-  st.write(' ... Vamos para adelante con Fabacti con toda ...')
-  st.write(' .. cambios ...')
-  st.write(' ... cambios 2 ...')
+  st.write(f" :blue[La TRM del día {fecha.strftime('%Y-%m-%d')} es de :red[{ftrm}]]")
+ 
+  st.write(' ...En construccion  ...')
+
+  st.write(":copyright: 2026 Todos los derechos reservados de autor :red[FABACTI] :registered:")
 
 if __name__ == '__main__':
     st.set_page_config(
