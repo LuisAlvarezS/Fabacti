cf6e3c7d2a63060340632d651c4fa76f549eca9a
LuisAlvarezS <86607669+LuisAlvarezS@users.noreply.github.com>
2026-05-13 23:04:06 +0000

    Trm

--- Fabacti.py
+++ Fabacti.py
@@ -2,21 +2,22 @@ import datetime
 
 import streamlit as st
 
-from suds.client import Client
+#from suds.client import Client
 
 from datetime import datetime
 
 # Funcion para consultar el TRM dada una fecha
 def obtener_trm(fecha):
     #URL de referencia para consulta de la TRM (Tasa Representativa del Mercado). Valor de referencia del dolar USA
-    URL_TRM = 'https://www.superfinanciera.gov.co/SuperfinancieraWebServiceTRM/TCRMServicesWebService/TCRMServicesWebService?WSDL'
+    #URL_TRM = 'https://www.superfinanciera.gov.co/SuperfinancieraWebServiceTRM/TCRMServicesWebService/TCRMServicesWebService?WSDL'
 
-    try:
-        client = suds.client.Client(URL_TRM, location = URL_TRM, faults=True)
-        trm =  client.service.queryTCRM(fecha)
-    except Exception as e:
-        return str(e)
-    return trm[4]
+    #try:
+        #client = suds.client.Client(URL_TRM, location = URL_TRM, faults=True)
+        #trm =  client.service.queryTCRM(fecha)
+    #except Exception as e:
+    #    return str(e)
+    trm = 3776.07
+    return trm
 
 
 def fabacti():
