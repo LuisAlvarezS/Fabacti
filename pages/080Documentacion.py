6c1166d31430a574525ab2b2ffe58cc9c06a697c
LuisAlvarezS <86607669+LuisAlvarezS@users.noreply.github.com>
2026-05-14 01:59:21 +0000

    Adiciona funciones documentacion

--- pages/080Documentacion.py
+++ pages/080Documentacion.py
@@ -1,4 +1,9 @@
 import streamlit as st
 
-st.write(" :red[NOTICIAS] :registered: :blue[Noticias y novedades sobre FABACTI]")
+st.write('<style>div.block-container{padding-top:2.5rem;}</style>', unsafe_allow_html=True)
+st.write("# :blue[Documentacion] :books: :books: :books: ")
+
+st.write(" En construccion :construction: :construction_worker: :construction_worker: :construction_worker: ")
+
+
 st.write(":copyright: 2024 Todos los derechos reservados de autor :red[FABACTI] :registered:")  
