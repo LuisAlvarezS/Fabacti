a12c2a84d566d63915375981b6f91bad390257b0
LuisAlvarezS <86607669+LuisAlvarezS@users.noreply.github.com>
2026-06-23 17:54:24 +0000

    Indicadores

--- pages/010Indicadores.py
+++ pages/010Indicadores.py
@@ -20,8 +20,9 @@ if 'usuario' in st.session_state:
         "Fecha": ["2019", "2020", "2021", "2022", "2023", "2024", "2025"],
         "Colombia": [67, 68, 67, 63, 66, 61, 71], 
         "Suiza": [65, 64, 62, 60, 58, 56, 54], 
+        "Chile": [60, 59, 57, 55, 53, 51, 49]
         })
  
-    st.line_chart(datos, x='Fecha', y=['Colombia', 'Suiza'], x_label='Fecha', y_label='Índice de Innovación', width='stretch')
+    st.line_chart(datos, x='Fecha', y=['Colombia', 'Suiza', 'Chile'], x_label='Fecha', y_label='Índice de Innovación', width='stretch')
 else:
     st.write(" :red[**Por favor inicie sesión para acceder a esta sección.**] ") 
