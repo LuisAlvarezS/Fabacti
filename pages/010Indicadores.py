9b0b4b2b065f0928837d6784c5cb348e95de0237
LuisAlvarezS <86607669+LuisAlvarezS@users.noreply.github.com>
2026-06-23 17:33:40 +0000

    Indicadores

--- pages/010Indicadores.py
+++ pages/010Indicadores.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 indice_global_innovacion = [ {"fecha": "2019", "valor": 67}, {"fecha": "2020", "valor": 68}, {"fecha": "2021", "valor": 67}, {"fecha": "2022", "valor": 63}, {"fecha": "2023", "valor": 66}, {"fecha": "2024", "valor": 61}, {"fecha": "2025", "valor": 71} ]
-st.line_chart(indice_global_innovacion, y='valor', x_label='fecha', y_label='Índice de Innovación', width='stretch')
+st.line_chart(indice_global_innovacion, y='valor', x = 'fecha', x_label='fecha', y_label='Índice de Innovación', width='stretch')
