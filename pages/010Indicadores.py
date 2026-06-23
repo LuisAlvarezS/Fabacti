d9def9c8ec31fd2b3c4487a3febc6cbae44b8ab6
LuisAlvarezS <86607669+LuisAlvarezS@users.noreply.github.com>
2026-06-23 17:46:58 +0000

    Indicadores

--- pages/010Indicadores.py
+++ pages/010Indicadores.py
@@ -1,3 +1,5 @@
+from turtle import pd
+
 import streamlit as st
 import constantes as co
 
@@ -10,6 +12,13 @@ if 'usuario' in st.session_state:
     st.subheader('Indice Global de Innovación')
     st.divider()
     indice_global_innovacion = [ {"fecha": "2019", "valor": 67}, {"fecha": "2020", "valor": 68}, {"fecha": "2021", "valor": 67}, {"fecha": "2022", "valor": 63}, {"fecha": "2023", "valor": 66}, {"fecha": "2024", "valor": 61}, {"fecha": "2025", "valor": 71} ]
-    st.line_chart(indice_global_innovacion, y='valor', x = 'fecha', x_label='fecha', y_label='Índice de Innovación', width='stretch')
+ 
+    datos = pd.DataFrame({
+        "Fecha": ["2019", "2020", "2021", "2022", "2023", "2024", "2025"],
+        "Colombia": [67, 68, 67, 63, 66, 61, 71], 
+        "Suiza": [65, 64, 62, 60, 58, 56, 54], 
+        })
+ 
+    st.line_chart(datos, x='Fecha', y=['Colombia', 'Suiza'], x_label='Fecha', y_label='Índice de Innovación', width='stretch')
 else:
     st.write(" :red[**Por favor inicie sesión para acceder a esta sección.**] ") 
