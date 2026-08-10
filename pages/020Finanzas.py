7ffbee6054a9ea11fefe9323fab37680d1bf2c8e
LuisAlvarezS <86607669+LuisAlvarezS@users.noreply.github.com>
2026-08-10 23:52:41 +0000

    Ajustes modulo de finanzas

--- pages/020Finanzas.py
+++ pages/020Finanzas.py
@@ -16,7 +16,7 @@ if 'usuario' in st.session_state:
     end_date = date.today().strftime("%Y-%m-%d")
 
     listaacciones = ['Google','Apple','Microsoft','Amazon','Tesla','Meta','CitiGroup', 'Alibaba','S&P 500','AT&T', 'Intel', 'CocaCola', 'Exxon Mobil', 'Walt Disney', 'Toyota','Cisco','Oracle','Nike']
-    listasimbolos = ['GOOGL', 'AAPL', 'MSFT', 'AMZN', 'TSLA', 'META', 'C', 'BABA', 'FB', 'T', 'INTC', 'K', 'XOM', 'DIS', 'TM', 'CSCO', 'ORCL', 'NKE']
+    listasimbolos = ['GOOGL', 'AAPL', 'MSFT', 'AMZN', 'TSLA', 'META', 'C', 'BABA', 'FB', 'T', 'INTC', 'KO', 'XOM', 'DIS', 'TM', 'CSCO', 'ORCL', 'NKE']
 
     datos = yf.Tickers('GOOGL AAPL MSFT AMZN TSLA META C BABA FB T INTC KO XOM DIS TM CSCO ORCL NKE')
     data = yf.download(listasimbolos, start_date2, end_date)
@@ -36,9 +36,18 @@ if 'usuario' in st.session_state:
                 datos11 = datos.tickers[simbolo1].info
                 empresa = datos11['symbol'] + "--" + datos11['shortName']
                 col1.write(f"{empresa}")
-                col1.write(f"**Precio:** USD {datos11['currentPrice']:.2f}")
-                col1.write(f"**Volumen:** {datos11['volume']:,}")
-                col1.write(f"**Capitalización de Mercado:** USD {datos11['marketCap']:,}")
+                try:
+                    col1.write(f"**Precio:** USD {datos11['currentPrice']:.2f}")
+                except:
+                    col1.write(f"**Precio:** No disponible")
+                try:
+                    col1.write(f"**Volumen:** {datos11['volume']:,}")
+                except:
+                    col1.write(f"**Volumen:** No disponible")
+                try:
+                    col1.write(f"**Capitalización de Mercado:** USD {datos11['marketCap']:,}")
+                except:
+                    col1.write(f"**Capitalización de Mercado:** No disponible")
                 stock1 = yf.Ticker(simbolo1)
                 data1 = stock1.history(start=start_date, end=end_date)
                 col1.line_chart(data1, y='Close', x_label = empresa, y_label= 'Precio USD', width='stretch')
@@ -51,9 +60,18 @@ if 'usuario' in st.session_state:
                 datos22 = datos.tickers[simbolo2].info
                 empresa = datos22['symbol'] + "--" + datos22['shortName']
                 col2.write(f"{empresa}")
-                col2.write(f"**Precio:** USD {datos22['currentPrice']:.2f}")
-                col2.write(f"**Volumen:** {datos22['volume']:,}")
-                col2.write(f"**Capitalización de Mercado:** USD {datos22['marketCap']:,}")
+                try:
+                    col2.write(f"**Precio:** USD {datos22['currentPrice']:.2f}")
+                except:
+                    col2.write(f"**Precio:** No disponible")
+                try:
+                    col2.write(f"**Volumen:** {datos22['volume']:,}")
+                except:
+                    col2.write(f"**Volumen:** No disponible")
+                try:
+                    col2.write(f"**Capitalización de Mercado:** USD {datos22['marketCap']:,}")
+                except:
+                    col2.write(f"**Capitalización de Mercado:** No disponible")
                 stock2 = yf.Ticker(simbolo2)
                 data2 = stock2.history(start=start_date, end=end_date)
                 col2.line_chart(data2, y='Close', x_label = empresa, y_label= 'Precio USD', width='stretch')
@@ -66,9 +84,18 @@ if 'usuario' in st.session_state:
                 datos33 = datos.tickers[simbolo3].info
                 empresa = datos33['symbol'] + "--" + datos33['shortName']
                 col3.write(f"{empresa}")
-                col3.write(f"**Precio:** USD {datos33['currentPrice']:.2f}")
-                col3.write(f"**Volumen:** {datos33['volume']:,}")
-                col3.write(f"**Capitalización de Mercado:** USD {datos33['marketCap']:,}")
+                try:
+                    col3.write(f"**Precio:** USD {datos33['currentPrice']:.2f}")
+                except:
+                    col3.write(f"**Precio:** No disponible")
+                try:
+                    col3.write(f"**Volumen:** {datos33['volume']:,}")
+                except:
+                    col3.write(f"**Volumen:** No disponible")
+                try:
+                    col3.write(f"**Capitalización de Mercado:** USD {datos33['marketCap']:,}")
+                except:
+                    col3.write(f"**Capitalización de Mercado:** No disponible")
                 stock3 = yf.Ticker(simbolo3)
                 data3 = stock3.history(start=start_date, end=end_date)
                 col3.line_chart(data3, y='Close', x_label = empresa, y_label= 'Precio USD', width='stretch')
